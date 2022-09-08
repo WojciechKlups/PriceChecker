5f90289a4266ea46bc709d7b64cdb87627ff2503
96peterharris <96peterharris@gmail.com>
2022-09-08 21:47:55 +0200

    import fix

--- src/main/java/org/wojciechklups/service/ResponsePreparer.java
+++ src/main/java/org/wojciechklups/service/ResponsePreparer.java
@@ -9,8 +9,9 @@ package org.wojciechklups.service;
 
 import org.wojciechklups.enums.ProductPageEnum;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.wojciechklups.WebClientPreparer.webClient;
 
